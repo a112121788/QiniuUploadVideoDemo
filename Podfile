620dd45308c5c684c72dc4a43b58304b550b3c1d
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-06-01 01:03:29 +0800

    use pod

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'QiniuUploadVideoDemo' do
+  use_frameworks!
+  pod 'Material'
+  pod 'SnapKit'
+  pod 'VGPlayer'
+  pod "Qiniu", "~> 7.2"
+
+end
---
+++ QiniuUploadVideoDemo/Demo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Demo-Bridging-Header.h
+//  QiniuUploadVideoDemo
+//
+//  Created by 高帅朋 on 2018/6/1.
+//  Copyright © 2018年 高帅朋. All rights reserved.
+//
+
+#ifndef Demo_Bridging_Header_h
+#define Demo_Bridging_Header_h
+
+#endif /* Demo_Bridging_Header_h */
+#import <Qiniu/QiniuSDK.h>
