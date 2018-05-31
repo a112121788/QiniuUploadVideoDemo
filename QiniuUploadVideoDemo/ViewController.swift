83a054ac3a3592824e4445f9fbe4739e30510c74
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-06-01 01:07:25 +0800

    remove Main.storyboard

--- QiniuUploadVideoDemo/ViewController.swift
+++ QiniuUploadVideoDemo/ViewController.swift
@@ -12,14 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "上传视频"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
