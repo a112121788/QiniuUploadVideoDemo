c6386b84d3d9c48f4000dbf9911dc648137ff3a3
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-06-01 00:56:12 +0800

    init project

---
+++ QiniuUploadVideoDemo/QiniuUploadVideoDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QiniuUploadVideoDemo
+//
+//  Created by 高帅朋 on 2018/6/1.
+//  Copyright © 2018年 高帅朋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
